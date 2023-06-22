2f4110eac92a8cd8bc9e7b2322f0e8f79514fe2c
matteobonesini <matteo.bonesini@gmail.com>
2023-06-22 12:37:23 +0200

    ADD random bombs generation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
+// function to generate 16 unique random numbers
+function uniqueRandomNumbers(max) {
+    const array = [];
+
+    while (array.length < 16) {
+        const randomN = Math.floor(Math.random() * max) + 1;
+
+        if (!array.includes(randomN)) {
+            array.push(randomN);
+        }
+    }
+
+    return array;
+
+}
+
 // reference to HTML element in page
 const createGridButton = document.getElementById('myapp--create-grid');
 const gridContainer = document.querySelector('.myapp--grid-container');
 const selectInput = document.getElementById('myapp--difficulty');
 
+
+
 createGridButton.addEventListener('click', function() {
 
     let numberOfCell;
 
+    // get params according to the difficulty
     switch(selectInput.value) {
         case "normal":
             numberOfCell = 81;
@@ -20,6 +39,10 @@ createGridButton.addEventListener('click', function() {
             numberOfCell = 100;
             document.documentElement.style.cssText = "--number-of-cell: 10";
     }
+
+    // generate the bombs array
+    const bombsArray = uniqueRandomNumbers(numberOfCell);
+    console.log(bombsArray);
 
     gridContainer.innerHTML = '';
 
